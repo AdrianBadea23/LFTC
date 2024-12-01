e7c89b67feb7b2bb78c4f3371bd2706095ab73c9
AdrianBadea23 <badeaadrian699@gmail.com>
2024-12-01 13:30:57 +0200

    Analiza de domeniu

--- parser.c
+++ parser.c
@@ -4,9 +4,14 @@
 #include <stdarg.h>
 
 #include "lexer.h"
+#include "ad.h"
 
-int iTk;	// the iterator in tokens
-Token *consumed;	// the last consumed token
+int iTk, blockDepth = 0;   // iterator in tokens
+Token *consumed;   // last consumed token
+
+// Domain management
+extern Domain *symTable;
+extern Symbol *crtFn;
 
 bool funcParams();
 bool funcParam();
@@ -22,357 +27,367 @@ bool exprPrefix();
 bool factor();
 bool defVar();
 bool program();
-
+bool addNewDomainIfNeeded();
+void removeDomainIfNeeded();
 
 // Same as err, but also prints the line of the current token
 _Noreturn void tkerr(const char *fmt, ...) {
-	fprintf(stderr, "error in line %d: ", tokens[iTk].line);
-	va_list va;
-	va_start(va, fmt);
-	vfprintf(stderr, fmt, va);
-	va_end(va);
-	fprintf(stderr, "\n");
-	exit(EXIT_FAILURE);
+    fprintf(stderr, "error in line %d: ", tokens[iTk].line);
+    va_list va;
+    va_start(va, fmt);
+    vfprintf(stderr, fmt, va);
+    va_end(va);
+    fprintf(stderr, "\n");
+    exit(EXIT_FAILURE);
 }
 
 bool consume(int code) {
-	if (tokens[iTk].code == code) {
-		consumed = &tokens[iTk++];
-		return true;
-	}
-	return false;
+    if (tokens[iTk].code == code) {
+        consumed = &tokens[iTk++];
+        return true;
+    }
+    return false;
 }
 
-// baseType ::= TYPE_INT | TYPE_REAL | TYPE_STR
 bool baseType() {
-	return consume(TYPE_INT) || consume(TYPE_REAL) || consume(TYPE_STR);
+    return consume(TYPE_INT) || consume(TYPE_REAL) || consume(TYPE_STR);
 }
 
-// defFunc ::= FUNCTION ID LPAR funcParams? RPAR COLON baseType defVar* block END
-bool defFunc() {
-	if (consume(FUNCTION)) {
-		if (consume(ID)) {
-			if (consume(LPAR)) {
-				funcParams();
-				if (consume(RPAR)) {
-					if (consume(COLON)) {
-						if (baseType()) {
-							while (defVar());
-							if (block()) {
-								if (consume(END)) {
-									return true;
-								} else {
-									tkerr("Expected end to close up the function");
-								}
-							} else {
-								tkerr("Expected code block after function");
-							}
-						} else {
-							tkerr("Expected function base type");
-						}
-					} else {
-						tkerr("Expected : after ) in function declaration");
-					}
-				} else {
-					tkerr("Expected ) to close the function");
-				}
-			} else {
-				tkerr("Expected ( after function identifier");
-			}
-		} else {
-			tkerr("Expected function identifier");
-		}
-	}
-
-	return false;
+bool isInsideBlock() {
+    return blockDepth > 0;
 }
 
-// funcParams ::= funcParam ( COMMA funcParam )*
-bool funcParams() {
-	if (funcParam()) {
-		while (consume(COMMA)) {
-			if (!funcParam()) {
-				tkerr("Error with function parameter after ,");
-			}
-		}
-		return true;
-	}
-	return false;
+bool addNewDomainIfNeeded() {
+    if (crtFn != NULL) {
+        addDomain();
+        
+    } else if (isInsideBlock()) {
+        
+    }
+    return true;
 }
 
-// funcParam ::= ID COLON baseType
+void removeDomainIfNeeded() {
+    
+    if (crtFn != NULL) {
+        delDomain();
+        printf("deletes the current domain\n");
+    } else if (isInsideBlock()) {
+        delDomain();
+        printf("deletes the current domain\n");
+    }
+}
+
+
+bool defVar() {
+    if (consume(VAR)) {
+        if (consume(ID)) {
+            const char *name = consumed->text;
+            if (consume(COLON)) {
+                if (baseType()) {
+                    if (searchInCurrentDomain(name)) {
+                        tkerr("Variable %s already defined in the current domain", name);
+                    }
+                    addSymbol(name, KIND_VAR);
+                    if (consume(SEMICOLON)) {
+                        return true;
+                    } else {
+                        tkerr("Expected ; after variable declaration");
+                    }
+                } else {
+                    tkerr("Expected base type after variable identifier");
+                }
+            } else {
+                tkerr("Expected : after variable identifier");
+            }
+        } else {
+            tkerr("Expected variable identifier");
+        }
+    }
+    return false;
+}
+
+
 bool funcParam() {
-	if (consume(ID)) {
-		if (consume(COLON)) {
-			if (baseType()) {
-				return true;
-			} else {
-				tkerr("Error with base type of function parameter");
-			}
-		} else {
-			tkerr("Expected : after identifier");
-		}
-	} else {
-
-	}
-	return false;
+    if (consume(ID)) {
+        const char *name = consumed->text;
+        if (consume(COLON)) {
+            if (baseType()) {
+                if (searchInCurrentDomain(name)) {
+                    tkerr("Parameter %s already defined in the current domain", name);
+                }
+				addSymbol(name, KIND_ARG);
+                addFnArg(crtFn, name);
+                return true;
+            } else {
+                tkerr("Expected base type for function parameter");
+            }
+        } else {
+            tkerr("Expected : after parameter identifier");
+        }
+    }
+    return false;
 }
 
-// block ::= instr+
+bool funcParams() {
+    if (funcParam()) {
+        while (consume(COMMA)) {
+            if (!funcParam()) {
+                tkerr("Error with function parameter after ,");
+            }
+        }
+        return true;
+    }
+    return false;
+}
+
+bool defFunc() {
+    if (consume(FUNCTION)) {
+        if (consume(ID)) {
+            const char *name = consumed->text;
+            if (searchInCurrentDomain(name)) {
+                tkerr("Function %s already defined", name);
+            }
+            crtFn = addSymbol(name, KIND_FN);
+            crtFn->args = NULL;
+            addNewDomainIfNeeded();
+
+            if (consume(LPAR)) {
+                funcParams(); 
+                if (consume(RPAR)) {
+                    if (consume(COLON)) {
+                        if (baseType()) {
+                            while (defVar());  
+                            if (block()) {  
+                                delDomain();  
+                                crtFn = NULL;
+                                if (consume(END)) {
+                                    return true;
+                                } else {
+                                    tkerr("Expected END to close the function");
+                                }
+                            } else {
+                                tkerr("Expected function block");
+                            }
+                        } else {
+                            tkerr("Expected base type for function return type");
+                        }
+                    } else {
+                        tkerr("Expected : after function parameter list");
+                    }
+                } else {
+                    tkerr("Expected ) to close function parameter list");
+                }
+            } else {
+                tkerr("Expected ( after function name");
+            }
+        } else {
+            tkerr("Expected function name");
+        }
+    }
+    return false;
+}
+
+
 bool block() {
-	if (instr()) {
-		while (instr());
-		return true;
-	}
-	return false;
+    if (instr()) {
+        while (instr());
+        return true;
+    }
+    return false;
 }
 
-/*
-instr ::= expr? SEMICOLON
-		| IF LPAR expr RPAR block ( ELSE block )? END
-		| RETURN expr SEMICOLON
-		| WHILE LPAR expr RPAR block END
-*/
 
 bool instr() {
-	if (consume(IF)) {
-		if (consume(LPAR) && expr() && consume(RPAR) && block()) {
-			if (consume(ELSE)) {
-				if (!block()) {
-					tkerr("Expected code block after else branch");
-				}
-			}
-
-			if (consume(END)) {
-				return true;
-			} else {
-				tkerr("Expected end after if statement");
-			}
-		} else {
-			tkerr("Malformed if statement");
-		}
-	} else if (consume(RETURN)) {
-		if (expr()) {
-			if (consume(SEMICOLON)) {
-				return true;
-			} else {
-				tkerr("Expected ; after expression");
-			}
-		} else {
-			tkerr("Expected expression after return");
-		}
-	} else if (consume(WHILE)) {
-		if (consume(LPAR) && expr() && consume(RPAR)){
-			if(block() && consume(END)) {
-				return true;
-			} else {
-				tkerr("Error in function code block or missing end");
-			}
-			
-		}else{
-			tkerr("Error in while statement expression");
-		}
-	} else if (expr()) {
-		if (consume(SEMICOLON)) {
-			return true;
-		} else {
-			tkerr("Expected ; after expression");
-		}
-	}
-	return false;
+    if (consume(IF)) {
+        if (consume(LPAR) && expr() && consume(RPAR)) {
+            if (block()) {
+                if (consume(ELSE)) {
+                    if (!block()) {
+                        tkerr("Expected code block after ELSE");
+                    }
+                }
+                if (consume(END)) {
+                    return true;
+                } else {
+                    tkerr("Expected END after IF statement");
+                }
+            } else {
+                tkerr("Expected block after IF condition");
+            }
+        } else {
+            tkerr("Malformed IF statement");
+        }
+    } else if (consume(RETURN)) {
+        if (expr()) {
+            if (consume(SEMICOLON)) {
+                return true;
+            } else {
+                tkerr("Expected ; after return expression");
+            }
+        } else {
+            tkerr("Expected expression after RETURN");
+        }
+    } else if (consume(WHILE)) {
+        if (consume(LPAR) && expr() && consume(RPAR)) {
+            if (block() && consume(END)) {
+                return true;
+            } else {
+                tkerr("Expected block and END after WHILE");
+            }
+        } else {
+            tkerr("Malformed WHILE statement");
+        }
+    } else if (expr()) {
+        if (consume(SEMICOLON)) {
+            return true;
+        } else {
+            tkerr("Expected ; after expression");
+        }
+    }
+    return false;
 }
+
 
 // expr ::= exprLogic
 bool expr() {
-	return exprLogic();
+    return exprLogic();
 }
 
 // exprLogic ::= exprAssign ( ( AND | OR ) exprAssign )*
 bool exprLogic() {
-	if (exprAssign()) {
-		while (consume(AND) || consume(OR)) {
-			if (!exprAssign()) {
-				tkerr("Expected expression after and/or");
-			}
-		}
-		return true;
-	}
-	return false;
+    if (exprAssign()) {
+        while (consume(AND) || consume(OR)) {
+            if (!exprAssign()) {
+                tkerr("Expected expression after AND/OR");
+            }
+        }
+        return true;
+    }
+    return false;
 }
 
 // exprAssign ::= ( ID ASSIGN )? exprComp
 bool exprAssign() {
-	if (consume(ID)) {
-		if (consume(ASSIGN)) {
-			return exprComp();
-		}
-		iTk--; // Backtrack since the assignment was not found
-	}
-	return exprComp();
+    if (consume(ID)) {
+        if (consume(ASSIGN)) {
+            return exprComp();
+        }
+        iTk--;
+    }
+    return exprComp();
 }
 
 // exprComp ::= exprAdd ( ( LESS | EQUAL ) exprAdd )?
 bool exprComp() {
-	if (!exprAdd()) {
-		return false;
-	}
-	if (consume(LESS) || consume(EQUAL)) {
-		if (!exprAdd()) {
-			return false;
-		}
-	}
-	return true;
+    if (!exprAdd()) {
+        return false;
+    }
+    if (consume(LESS) || consume(EQUAL)) {
+        if (!exprAdd()) {
+            tkerr("Expected expression after comparison operator");
+        }
+    }
+    return true;
 }
 
 // exprAdd ::= exprMul ( ( ADD | SUB ) exprMul )*
 bool exprAdd() {
-	if (!exprMul()) {
-		return false;
-	}
-	while (consume(ADD)) {
-		if (!exprMul()) {
-			tkerr("Expected variable when performing addition");
-			return false;
-		}
-	}
-
-	while (consume(SUB)) {
-		if (!exprMul()) {
-			tkerr("Expected variable when performing subtraction");
-			return false;
-		}
-	}
-	return true;
+    if (!exprMul()) {
+        return false;
+    }
+    while (consume(ADD) || consume(SUB)) {
+        if (!exprMul()) {
+            tkerr("Expected term after addition/subtraction operator");
+        }
+    }
+    return true;
 }
 
 // exprMul ::= exprPrefix ( ( MUL | DIV ) exprPrefix )*
 bool exprMul() {
-	if (!exprPrefix()) {
-		return false;
-	}
-		while (consume(MUL)) {
-		if (!exprMul()) {
-			tkerr("Expected variable when performing multiplication");
-			return false;
-		}
-	}
-
-	while (consume(DIV)) {
-		if (!exprMul()) {
-			tkerr("Expected variable when performing division");
-			return false;
-		}
-	}
-	return true;
+    if (!exprPrefix()) {
+        return false;
+    }
+    while (consume(MUL) || consume(DIV)) {
+        if (!exprPrefix()) {
+            tkerr("Expected term after multiplication/division operator");
+        }
+    }
+    return true;
 }
 
 // exprPrefix ::= ( SUB | NOT )? factor
 bool exprPrefix() {
-	if (consume(SUB) || consume(NOT)) {
-		if (!factor()) {
-			return false;
-		}
-	} else {
-		if (!factor()) {
-			return false;
-		}
-	}
-	return true;
+    if (consume(SUB) || consume(NOT)) {
+        if (!factor()) {
+            tkerr("Expected expression after prefix operator");
+        }
+    } else {
+        if (!factor()) {
+            return false;
+        }
+    }
+    return true;
 }
 
 /*
 factor ::= INT
-		| REAL
-		| STR
-		| LPAR expr RPAR
-		| ID ( LPAR ( expr ( COMMA expr )* )? RPAR )?
+        | REAL
+        | STR
+        | LPAR expr RPAR
+        | ID ( LPAR ( expr ( COMMA expr )* )? RPAR )?
 */
 bool factor() {
-	if (consume(INT)) {
-		return true;
-	}
-	if (consume(REAL)) {
-		return true;
-	}
-	if (consume(STR)) {
-		return true;
-	}
-	if (consume(LPAR)) {
-		if (expr()) {
-			if (consume(RPAR)) {
-				return true;
-			} else {
-				tkerr("Missing closing ) in factor");
-			}
-		} else {
-			tkerr("Invalid expression inside ()");
-		}
-	}
-	if (consume(ID)) {
-		if (consume(LPAR)) {
-			if (expr()) {
-				while (consume(COMMA)) {
-					if (!expr()) {
-						tkerr("Invalid expression after ,");
-					}
-				}
-				if (consume(RPAR)) {
-					return true;
-				} else {
-					tkerr("Missing ) for function call");
-				}
-			} else {
-				tkerr("Invalid expression in function call");
-			}
-		} else {
-			return true;
-		}
-	}
-	return false;
+    if (consume(INT) || consume(REAL) || consume(STR)) {
+        return true;
+    }
+    if (consume(LPAR)) {
+        if (expr()) {
+            if (consume(RPAR)) {
+                return true;
+            } else {
+                tkerr("Expected closing ')' in factor");
+            }
+        } else {
+            tkerr("Expected expression in parentheses");
+        }
+    }
+    if (consume(ID)) {
+        if (consume(LPAR)) {
+            if (expr()) {
+                while (consume(COMMA)) {
+                    if (!expr()) {
+                        tkerr("Expected expression after ',' in function call");
+                    }
+                }
+                if (consume(RPAR)) {
+                    return true;
+                } else {
+                    tkerr("Expected closing ')' in function call");
+                }
+            }
+        } else {
+            return true;
+        }
+    }
+    return false;
 }
 
-// defVar ::= VAR ID COLON baseType SEMICOLON
-bool defVar() {
-	if (consume(VAR)) {
-		if (consume(ID)) {
-			if (consume(COLON)) {
-				if (baseType()) {
-					if (consume(SEMICOLON)) {
-						return true;
-					} else {
-						tkerr("Expected ; after declaration");
-					}
-				} else {
-					tkerr("Expected base type after identifier");
-				}
-			} else {
-				tkerr("Expected , after variable");
-			}
-		} else {
-			tkerr("Expected variable identifier");
-		}
-	}
 
-	return false;
-}
-
-// program ::= ( defVar | defFunc | block )* FINISH
 bool program() {
-	for (;;) {
-		if (defVar()) {}
-		else if (defFunc()) {}
-		else if (block()) {}
-		else break;
-	}
-	if (consume(FINISH)) {
-		return true;
-	} else {
-		tkerr("syntax error");
-	}
-	return false;
+    addDomain();
+    while (defVar() || defFunc() || block());
+    delDomain();
+    if (consume(FINISH)) {
+        return true;
+    } else {
+        tkerr("Syntax error");
+    }
+    return false;
 }
 
 void parse() {
-	iTk = 0;
-	program();
+    iTk = 0;
+    program();
 }
